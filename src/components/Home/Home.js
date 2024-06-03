f84a008412f3d900c877c535fc863e74dd9c03bc
Abhishekp12 <abhiec.2011@gmail.com>
2024-06-03 14:48:02 +0530

    dded dummy popup bootstrap modal

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import './Home.css'
+import React,{useState} from 'react';
+import Button from 'react-bootstrap/Button';
+import './Home.css';
+import MyVerticallyCenteredModal from'../Modal/Modal';
 const Home = () => {
+  const [modalShow, setModalShow] = useState(false);
   return (
+    <>
     <div  className='conatcCont'>
      <div>
      <h1>Home</h1>
+  
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Launch vertically centered modal
+      </Button>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+ 
     </div>   
     </div>
+    </>
   )
 }
 
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,50 @@
+
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useNavigate } from 'react-router-dom';
+
+function MyVerticallyCenteredModal(props) {
+    const navigate= useNavigate();
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button onClick={()=>navigate('/')}variant="primary" type="submit" >
+        Submit
+      </Button>
+    </Form>
+      </Modal.Body>
+      {/* <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer> */}
+    </Modal>
+  );
+}
+export default MyVerticallyCenteredModal
